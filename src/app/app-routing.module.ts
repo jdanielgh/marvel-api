cf9d40f1fc2ebd1a7073199653ba416d018089d4
Jesus Daniel Gomez Hernandez <jesus.gomezher@gmail.com>
2021-11-29 11:27:25 -0500

    add routing configuration for deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [ { path: '', redirectTo: '/character', pathMatch: 'full'
 }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
